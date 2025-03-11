b906966c9e4e89fa2a2a784c8d9df2ea3ecf4b91
HellcatOG <pranavunnikrishnan109@gmail.com>
2025-03-11 10:27:11 +0530

    mwssage

--- build_files.sh
+++ build_files.sh
@@ -9,3 +9,19 @@ echo "Building the project..."
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
+
+#!/bin/bash
+
+# Exit on error
+set -e
+
+# Build the project
+echo "Collecting static files..."
+python manage.py collectstatic --no-input
+
+echo "Running migrations..."
+python manage.py makemigrations
+python manage.py migrate
+
+# Make script executable
+chmod +x build_files.sh
